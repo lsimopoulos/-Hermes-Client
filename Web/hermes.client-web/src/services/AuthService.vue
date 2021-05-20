2e5ac51abe7c8155a45049729af7353179259174
Leonidas Simopoulos <leonidas.simopoulos@gmail.com>
2021-05-20 14:01:46 +0200

    Added the intiial draft version for vue client

---
+++ Web/hermes.client-web/src/services/AuthService.vue
@@ -0,0 +1,12 @@
+import { createOidcAuth, SignInType } from 'vue-oidc-client/vue3';
+
+const appUrl = 'https://localhost:55555/';
+
+const tokenClient = createOidcAuth('main', SignInType.Window, appUrl , {
+  authority: 'https://localhost:7000/',
+  client_id: 'chat_web_client',
+  response_type: 'id_token token',
+  scope: 'hermes'
+});
+
+export default tokenClient;
