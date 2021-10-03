5d3eeebff5263dd74837fd5cfb2f635d60288ac4
Anastasios Pavlis <pavanas0@gmail.com>
2021-10-03 21:43:42 +1000

    addimg grpc initial

--- Web/hermes.client-web/src/components/Chat.vue
+++ Web/hermes.client-web/src/components/Chat.vue
@@ -11,6 +11,17 @@
 <script>
 //   import ChatWindow from 'vue-advanced-chat';
   import 'vue-advanced-chat/dist/vue-advanced-chat.css';
+  import { SendRequest } from '../proto/hermes_pb';
+  import { ChatterClient } from '../proto/hermes_grpc_web_pb';
+
+  const client = new ChatterClient('http://localhost:8080');
+
+  const message = new SendRequest();
+  message.setMessage("WOW");
+
+  client.chat(message, {}, (err, response) => {
+    console.log(response.getMessage());
+  });
 
   export default {
     name: "Chat",
