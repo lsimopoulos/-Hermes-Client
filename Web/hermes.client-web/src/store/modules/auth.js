9fb820612a42b6a78d5874bbd47a670c76000327
Leonidas Simopoulos <leonidas.simopoulos@gmail.com>
2021-11-01 11:10:20 +0100

    fixed som syntax mstaikes

--- Web/hermes.client-web/src/store/modules/auth.js
+++ Web/hermes.client-web/src/store/modules/auth.js
@@ -16,49 +16,47 @@ export const auth = {
             console.log(err);
             commit('loginFailure');
             reject(err);
-          });
-      })
+          })
+      });
     },
     logout({ commit }) {
       commit('logout');
     },
     attemptRegister({ commit }, { email, password }) {
       return new Promise((resolve, reject) => {
-        AuthService.register(email, password)
+        AuthService.register({ email, password })
           .then(response => {
             commit('registerSuccess');
-            return Promise.resolve(response.data);
-          },
-            error => {
-              commit('registerFailure');
-              return Promise.reject(error);
-            }
-          );
-      }
-  },
-    mutations: {
-      loginSuccess(state, response_token) {
-        state.status.loggedIn = true;
-        state.response_token = response_token;
-      },
-      loginFailure(state) {
-        state.status.loggedIn = false;
-        state.response_token = null;
-      },
-      logout(state) {
-        state.status.loggedIn = false;
-        state.response_token = null;
-      },
-      registerSuccess(state) {
-        state.status.loggedIn = false;
-      },
-      registerFailure(state) {
-        state.status.loggedIn = false;
-      }
-    },
-    getters: {
-      access_token: state => state.token.access_token,
-      refresh_token: state => state.token.refresh_token
+            resolve(response.data);
+          }).catch((error) => {
+            commit('registerFailure');
+            reject(error);
+          })
+      });
     }
-
-  };
+  },
+  mutations: {
+    loginSuccess(state, response_token) {
+      state.status.loggedIn = true;
+      state.response_token = response_token;
+    },
+    loginFailure(state) {
+      state.status.loggedIn = false;
+      state.response_token = null;
+    },
+    logout(state) {
+      state.status.loggedIn = false;
+      state.response_token = null;
+    },
+    registerSuccess(state) {
+      state.status.loggedIn = false;
+    },
+    registerFailure(state) {
+      state.status.loggedIn = false;
+    }
+  },
+  getters: {
+    access_token: state => state.token.access_token,
+    refresh_token: state => state.token.refresh_token
+  }
+};
