929a6b9598b66154e97cf68e5e3845ec32733547
Leonidas Simopoulos <Leonidas.Simopoulos@rco.se>
2022-04-18 09:40:03 +0200

    working progress

--- Web/hermes.client-web/src/components/ChatComponent.vue
+++ Web/hermes.client-web/src/components/ChatComponent.vue
@@ -1,51 +1,65 @@
 <template>
-    <div class="col-md-12">
-        <!-- <chat-window
-            :current-user-id="currentUserId"
-            :rooms="rooms"
-            :messages="messages"
-        /> -->
-        Hello!!
-    </div>
+  <div class="col-md-12"></div>
+  
+  <textarea  v-model="chatMsg" />
+   <input type="submit" class="btn btn-primary" @click="sendMessage"/>
 </template>
 <script>
 //   import ChatWindow from 'vue-advanced-chat';
   // import 'vue-advanced-chat/dist/vue-advanced-chat.css';
-  import { SendRequest } from '../proto/hermes_pb';
+  import { sendRequest } from '../proto/hermes_pb';
   import { ChatterClient } from '../proto/hermes_grpc_web_pb';
   import { CallCredentials } from '@grpc/grpc-js/build/src/call-credentials'
   
 
   export default {
     name: "ChatComponent",
+  
      created () {
    
-    this.connect();
+   
+  },
+  mounted()
+  {
+    this.connect()
   },
     components: {
     //   ChatWindow
     },
     methods : {
    connect () {
-       const token = this.$store.getters['auth/access_token'];
-      const metadata = { 'authorization': 'Bearer ' + token };
-      var request = new SendRequest();
-      request.setMessage("Wow");
+      
        this.client = new ChatterClient('https://localhost:5001', CallCredentials.createFromPlugin, null);
-      this.client.Chat(request, metadata, (err, response) => {
-        if (err) {
-          console.log(`Unexpected error for sayHello: code = ${err.code}, message = "${err.message}"`)
-        } else {
-          console.log(response.getMessage())
-        }
-      })
-    }
+   
+   },
+   sendMessage()
+   {
+
+      const token = this.$store.getters['auth/access_token'];
+      const metadata = { 'Authorization': 'Bearer ' + token , 'Content-Type' :'application/grpc-web-text'};
+      var request = new sendRequest();
+      request.setMessage(this.chatMsg);
+      
+      var  call =  this.client.chat(request,metadata);
+     call.on('data', function (result) {
+       
+       console.log(result)
+       });
+
+        call.on('status', function (status) {
+          
+          console.log(status.code, status.details, status.metadata);
+    });
+
+    call.on("end", () => {
+      console.log("Stream ended.");
+    });
+   }
+    
     },
     data() {
       return {
-        rooms: [],
-        messages: [],
-        currentUserId: 1234
+        chatMsg: ""
       }
     }
   }
